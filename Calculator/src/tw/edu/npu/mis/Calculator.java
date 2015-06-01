241690403533166bcca990b917f5e16b1b47475a
a8342222a <s860640@gmail.com>
2015-06-01 17:24:36 +0800

    計算機
    新增計算機版面

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -53,8 +53,9 @@ public class Calculator {
     /**
      * @param args the command line arguments
      */
+    /*
     public static void main(String[] args) {
         // TODO code application logic here
     }
-
+*/
 }
